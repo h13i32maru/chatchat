c647747695c659ee6a81b163b06c73eae5ff5095
KeisukeUtsumi <void.of.glans@gmail.com>
2013-01-22 10:21:11 +0900

    インフォメーション機能を追加しました。

--- src/background.js
+++ src/background.js
@@ -4,3 +4,31 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     });
   });
 });
+
+//コンテキストメニューに追加
+var chatworkContext = chrome.contextMenus.create(
+  {
+    "title" : "ChatWorkUtil",
+    "contexts":["selection"],
+    "documentUrlPatterns":[
+      "https://kcw.kddi.ne.jp/*",
+      "https://www.chatwork.com/*",
+    ],
+  }
+);
+//infomationHelper
+chrome.contextMenus.create({
+  "title" : "インフォメーション",
+  "contexts": ["selection"], 
+  "parentId" : chatworkContext,
+  "onclick": infomationHelper
+});
+function infomationHelper(info, tab) {
+  chrome.tabs.executeScript(
+    null, 
+    {
+      file: "infomation.js", 
+      allFrames: true
+    }
+  );
+}
